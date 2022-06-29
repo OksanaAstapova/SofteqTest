9c20ad4008c24ce845d0e6f6345c18d564aed0d5
Oksana Astapova <astapova.frontend@gmail.com>
2022-06-29 15:02:32 +0300

    feat: add the solution for the second task (Wed, Jun 29, 2022 03:02:31 PM)

---
+++ test2/index.js
@@ -0,0 +1,6 @@
+for (let i = 1; i<100; i++){
+    if (i % 3 == 0 && i % 7 == 0){console.log (`OpenSource`)}
+    else if(i % 3 == 0){console.log(`Open`)}
+    else if(i % 7 == 0) {console.log(`Source`)}
+   else console.log(i);
+}
