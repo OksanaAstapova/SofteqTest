9bf2a07d676f975189a3a2c075cbf8affcd8ddea
Oksana Astapova <astapova.frontend@gmail.com>
2022-06-29 22:42:34 +0300

    feat: add the solution for the fifth task (Wed, Jun 29, 2022 10:42:34 PM)

--- test5/test5.js
+++ test5/test5.js
@@ -1,2 +1,14 @@
-let requestURL = 'https://jsonplaceholder.typicode.com/posts';
-let request = new XMLHttpRequest();
+function getObject(){
+    let requestURL = 'https://jsonplaceholder.typicode.com/posts';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+      let objects = request.response;
+      let object = objects.find(x => x.id === 5);
+      console.log(object)
+      return object;
+      
+    }
+  }
